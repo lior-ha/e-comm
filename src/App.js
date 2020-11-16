3a5eadd4f10386965f825eba113fb30494506fd2
Lior <liorha@gmail.com>
2020-11-16 10:41:06 +0200

    Update menu-item to navigagte withRouter HOC (Higher Order Component)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-function App() {
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+)
+
+function App() { 
   return (
     <div>
-        <HomePage />
+        <Switch> {/* Switch - Optionl - Makes the code render the first hit from the Routes */}
+            <Route exact path='/' component={HomePage} />
+            <Route path='/hats' component={HatsPage} />
+        </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,12 @@
+// withRouter is a higher order component. It is a function tahat takes a component and returns a modified one
+// In this case it gives access to match, location and history from the Router component
+import { withRouter } from 'react-router-dom';
+
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
     return (
-        <div className={`${size} menu-item`}>
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
             <div 
                 className="background-image"
                 style={{
@@ -18,4 +22,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
     )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
