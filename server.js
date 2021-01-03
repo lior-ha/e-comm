74a67696324f1ef561bf5ae20c17d143650d8385
Lior <liorha@gmail.com>
2021-01-03 10:35:40 +0200

    trying again - MIME type fix take 6

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
     
     app.get('/service-worker.js', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'service-worker.js'));
     });
 
     app.get('*', function(req, res) {
